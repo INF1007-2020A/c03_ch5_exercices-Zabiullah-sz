10df12e275d923a468b84011df4a6cc06aa42113
Zabiullah-sz <71023764+Zabiullah-sz@users.noreply.github.com>
2020-10-13 16:59:14 -0400

    Update exercice.py

--- exercice.py
+++ exercice.py
@@ -13,43 +13,47 @@ def convert_to_absolute() -> float:
     else:
         return number
 
+
 def use_prefixes() -> List[str]:
+    result = []
     prefixes, suffixes = 'JKLMNOP', 'ack'
-    for letter in prefixes():
+    for letter in prefixes:
         mot = letter + suffixes
-        return mot
-    
+        result.append(mot)
+    return result
 
 
 def prime_integer_summation() -> int:
-nombrePremier = 0
-nombre = 2
-sommes = 0
-while(nombrePremier < 100):
-    for i in range(1, nombre+1):
-        if (nombre % i == 0 and i != 1 and i != nombre):
-            break
-        elif(i == nombre):
+    nombrePremier = 0
+    nombre = 2
+    sommes = 0
+    while (nombrePremier < 100):
+        estPremier = True
+        for i in range(1, nombre + 1):
+            if (nombre % i == 0 and i != 1 and i != nombre):
+                estPremier = False
+                break
+        if estPremier:
             nombrePremier += 1
             sommes += nombre
-    nombre += 1
-print(sommes)
+        nombre += 1
+    return sommes
 
 
 def factorial(number: int) -> int:
-if number != 0:
-    for i in range(number):
-        number *= (number-1)
-        print(number)
-
+    if number != 0:
+        for i in range(number):
+            number *= (number - 1)
+    return number
 
 
 def use_continue() -> None:
-    for i in range (1,11):
-        if i==5:
+    for i in range(1, 11):
+        if i == 5:
             continue
         else:
             print(i)
+
 
 def main() -> None:
     print(f"La valeur absolue du nombre est {convert_to_absolute()}")
@@ -60,7 +64,7 @@ def main() -> None:
 
     number = 10
     print(f"La factiorelle du nombre {number} est: {factorial(number)}")
-    
+
     print(f"L'affichage de la boucle est:")
     use_continue()
 
